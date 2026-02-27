2f10c958fa6ff9f38ea4d605cb927dcfbd50ed29
FORTIN Victor <victor.fortin078@gmail.com>
2026-02-27 00:53:47 -0500

    Header + footer, test si tout marche

---
+++ app/views/header.php
@@ -0,0 +1,8 @@
+<html lang="fr">
+    <head>
+        <link href="assets/style.css" rel="stylesheet">
+        <title>ToDoList</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    ok
