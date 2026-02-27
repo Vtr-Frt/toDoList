add403afb5fd0a06d234248549c50bccb0b36038
FORTIN Victor <victor.fortin078@gmail.com>
2026-02-27 09:26:21 -0500

    ajout view connexion

--- app/views/header.php
+++ app/views/header.php
@@ -8,7 +8,7 @@
     <div class="banniere">
         <h1>To Do List</h1>
         <nav>
-            <a href="public/index.php?action=connexion">Connexion</a>
-            <a href="public/index.php?action=inscrire">Inscrire</a>
+            <a href="index.php?action=connexion">Connexion</a>
+            <a href="index.php?action=inscrire">Inscrire</a>
         </nav>  
     </div>
--- public/index.php
+++ public/index.php
@@ -4,4 +4,8 @@ session_start();
 
 require __DIR__ . '/../app/views/header.php';
 
+if($_GET['action'] == 'connexion'){
+    require __DIR__ . '/../app/views/connexion.php';
+}
+
 require __DIR__ . '/../app/views/footer.php';
