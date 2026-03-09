497cb63a86efbcf1e1585354121bbc8295a2e1a6
FORTIN Victor <victor.fortin078@gmail.com>
2026-03-08 22:25:24 -0400

    Add gitignore

--- app/views/compte.php
+++ app/views/compte.php
@@ -3,5 +3,5 @@
 <img src="" alt="Image de profil">
 
 <h2><?=  $_SESSION['username'] ?></h2>
-<p>Id groupe : </p>
+<p>Id groupe : <?= htmlspecialchars() ?> </p>
 <a class="btn" href="index.php?action=updateProfil">Modifier profil</a>
